8fbacf6e091f3a46242e8d9eab7365fa5edeb540
Rajeswari Karthik <rajijan12@gmail.com>
2022-09-22 17:19:02 -0400

    first commit for basic java

---
+++ src/test/java/simplemaven/TestProgram.java
@@ -0,0 +1,19 @@
+package simplemaven;
+
+import org.testng.annotations.Test;
+
+public class TestProgram {
+	
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("sample test execution");
+	}
+
+	public static void main(String[] args) {
+	
+		System.out.println("sample test execution");
+
+	}
+
+}
